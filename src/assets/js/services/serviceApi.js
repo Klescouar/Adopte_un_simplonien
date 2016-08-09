8e6610d59def5756c5b246aad6459d602f0b1ed3
J2A2B <ja.andreanigmail.com>
2016-08-09 17:29:51 +0200

    add card simplonien BO

--- src/assets/js/services/serviceApi.js
+++ src/assets/js/services/serviceApi.js
@@ -152,5 +152,8 @@ app.service("serviceApi", function(){
 
 
   this.createUser = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/create/user';
+  this.getUser = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/user';
+  this.deleteUser = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/delete/user/';
+  this.createStudent = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/create/simplonien';
 
 });
