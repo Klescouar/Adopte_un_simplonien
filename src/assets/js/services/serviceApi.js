649d0166c20ae97cace8249d36e4761d21bb498f
Le scouarnec Kevin <kevin_lescouarnec@DX-CZC14056XD.groupevsc.com>
2016-08-26 16:35:48 +0200

    Add some details

--- src/assets/js/services/serviceApi.js
+++ src/assets/js/services/serviceApi.js
@@ -153,12 +153,12 @@ app.service("serviceApi", function(){
       phone: '',
       active: false,
   }];
-  this.createUser = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/create/user';
-  this.getUser = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/user';
-  this.deleteUser = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/delete/user/';
-  this.createStudent = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/create/simplonien';
-  this.getSimplonien = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/listeSimplonien';
-  this.deleteSimplonien = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/delete/simplonien/';
-  this.modifySimplonien = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/simplonien/';
-  this.modifySendSimplonien = 'http://localhost:8888/Adopte_un_simplonien/server/index.php/api/modify/simplonien/';
+  this.createUser = 'http://localhost/LAMP/Adopte_un_simplonien/server/index.php/api/create/user';
+  this.getUser = 'http://localhost/LAMP/Adopte_un_simplonien/server/index.php/api/user';
+  this.deleteUser = 'http://localhost/LAMP/Adopte_un_simplonien/server/index.php/api/delete/user/';
+  this.createStudent = 'http://localhost/LAMP/Adopte_un_simplonien/server/index.php/api/create/simplonien';
+  this.getSimplonien = 'http://localhost/LAMP/Adopte_un_simplonien/server/index.php/api/listeSimplonien';
+  this.deleteSimplonien = 'http://localhost/LAMP/Adopte_un_simplonien/server/index.php/api/delete/simplonien/';
+  this.modifySimplonien = 'http://localhost/LAMP/Adopte_un_simplonien/server/index.php/api/simplonien/';
+  this.modifySendSimplonien = 'http://localhost/LAMP/Adopte_un_simplonien/server/index.php/api/modify/simplonien/';
 });
