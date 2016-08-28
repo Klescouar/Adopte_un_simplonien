345f1082ee1e8062d8d36a62fa9af76d3378ace1
Kevin Le Scouarnec <lescouarneckevin@yahoo.fr>
2016-08-28 13:05:44 +0200

    ProjectPage amelioration

--- src/assets/js/Jquery/scrollSticky.js
+++ src/assets/js/Jquery/scrollSticky.js
@@ -1,20 +1,4 @@
 $(document).ready(function() {
-    $(function() {
-        var nav = $('.filterMain');
-        if (nav.length) {
-            var stickyNavTop = nav.offset().top + 4;
-            $(window).scroll(function() {
-                if ($(window).scrollTop() > stickyNavTop && screen.width > 640) {
-                    $('.filterMain').addClass('sticktotop');
-                    $('.cardPage').addClass('marginToFix');
-                } else {
-                    $('.filterMain').removeClass('sticktotop');
-                    $('.cardPage').removeClass('marginToFix');
-                }
-            });
-        }
-    });
-
     $('#menu-button').on('click', function() {
         $('.burgerMenu').toggleClass('menu-open');
     });
--- src/views/project.php
+++ src/views/project.php
@@ -1,21 +1,22 @@
 <div class="cont-simplon-description" ng-controller="projectCtrl">
-	<p class="title-simplon" ng-click="showTop()" ng-class="{'title-simplon-closed': showDescription}">SIMPLON.CO ?</p>
-	<div class="cont-img-description cont-img" ng-class="{'cont-img-description-simple':showDescription}">
+	<p class="title-simplon" ng-click="showTop()" ng-class="{'title-simplon-closed': showDescription}">Qu'est ce que simplon.co ?</p>
+	<div class="cont-img-description cont-img" ng-click="showTop()" ng-class="{'cont-img-description-simple':showDescription}">
 		<div class="mask" ng-class="{'mask-is-open': showDescription}">
-			<div class="cross-close" ng-click="showTop()"></div>
+			<div class="cross-close"></div>
 		</div>
 		<div class="generic-cont-description description-simlpon"  ng-class="{'description-simlpon-is-open': showDescription}">
-			<p class="corpus-description">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
+			<p class="corpus-description">
+Simplon.co est une entreprise de l’économie sociale et solidaire (agrément ESUS) : ses formations sont qualifiantes ou certifiantes, certaines sont labellisées Grande Ecole du Numérique, mais toutes sont ouvertes sur critères sociaux sans aucun pré-requis technique (débutants acceptés) mais il est obligatoire d’avoir une très forte MOTIVATION, une appétence pour le numérique (et la programmation) et d’aimer travailler en équipe ! </p>
 			<p class="end-description">FABRIQUE DE TALENTS 3.0</p>
 		</div>
 		<div class="mask2" ng-class="{'mask2-is-open': showDescription}"></div>
 	</div>
 	<p class="title-recrutement" ng-click="showDown()" ng-class="{'title-recrutement-closed': showDescription2}">ADOPTEZ UN SIMLONIEN</p>
-	<div class="cont-img-description-down cont-img">
+	<div class="cont-img-description-down cont-img" ng-click="showDown()">
 		<div class="mask" ng-class="{'mask-is-open': showDescription2}">
-		<div class="cross-close" ng-click="showDown()"></div>
+		<div class="cross-close"></div>
 		</div>
-		<div class="generic-cont-description description-simlpon" onscroll="descript()" ng-class="{'description-simlpon-is-open': showDescription2}">
+		<div class="generic-cont-description description-simlpon" ng-class="{'description-simlpon-is-open': showDescription2}">
 			<p class="corpus-description">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
 			<p class="end-description">FABRIQUE DE TALENTS 3.0</p>
 		</div>
