db8e29127382ff8b97a81305211e46ad00f68365
Kevin Le Scouarnec <lescouarneckevin@yahoo.fr>
2016-08-28 17:24:55 +0200

    Fix buttonContainer on backOffice problem

--- src/assets/js/controllers/profilCtrl.js
+++ src/assets/js/controllers/profilCtrl.js
@@ -5,7 +5,7 @@ app.controller('profilCtrl', ['$scope', '$http', 'serviceApi', '$routeParams', f
         .then(
             function(response) {
                 $scope.student = response.data;
-                console.log($scope.student[0].Linkedin);
+                console.log($scope.student[0].Domaine);
 
             },
             function(err) {
