394652d404f1d32eecd62ffa43feacdb7945734d
J2A2B <ja.andreani@gmail.com>
2016-08-28 16:39:39 +0200

    pull BO delete file

--- src/assets/js/controllers/profilCtrl.js
+++ src/assets/js/controllers/profilCtrl.js
@@ -1,11 +1,12 @@
 app.controller('profilCtrl', ['$scope', '$http', 'serviceApi', '$routeParams', function($scope, $http, serviceApi, $routeParams) {
 
     var id = $routeParams.id_profil;
+    console.log(id)
     $http.get(serviceApi.profilUser + id)
         .then(
             function(response) {
                 $scope.student = response.data;
-                console.log($scope.student[0].Photo);
+                console.log($scope.student);
 
             },
             function(err) {
