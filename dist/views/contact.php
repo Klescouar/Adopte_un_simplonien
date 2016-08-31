0e0a681405408c68f041b9443255ce47433f3e36
Le scouarnec Kevin <kevin_lescouarnec@DX-CZC14056XD.groupevsc.com>
2016-08-31 11:42:27 +0200

    Tentative page contact

--- dist/views/contact.php
+++ dist/views/contact.php
@@ -1,8 +1,55 @@
 <div class="contactPage" ng-controller="contactCtrl">
     <div class="simplonPicture">
+            <h1>Une question, une suggestion, un doute ? C’est par ici !</h1>
+    </div>
+    <div class="contactDown">
+        <div class="mapContact">
+            <div id="googleMap"></div>
+        </div>
+        <div class="adressSimplon">
+            <h1>Simplon Montreuil</h1>
+            <p>55 rue de Vincennes</p>
+            <p>93100 Montreuil</p>
+            <p>inbox@simplon.co</p>
+        </div>
+        <div class="contactForm animated slideInUp">
+        <form method="post">
+            <input type="text" name="Nom" class="Name" placeholder="Nom/Prenom *" required/>
+            <input type="text" name="Nom" class="Name" placeholder="Entreprise *" required/>
+            <input type="text" name="City" class="City" placeholder="Ville *" required/>
+            <input type="email" name="Email" class="Email" placeholder="Email *" required/>
+            <input type="text" name="Phone" class="Phone" placeholder="Telephone" />
+            <textarea name="Message" rows="20" cols="20" class="Message" placeholder="Message *" required></textarea>
+            <input type="submit" name="submit" value="ENVOYER" class="submit-button" />
+        </form>
+    </div>
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <div class="contactPage" ng-controller="contactCtrl">
+    <div class="simplonPicture">
         <div class="contactForm animated slideInUp">
             <h1>Une question, une suggestion, un doute ? C’est par ici !</h1>
-            <!-- <p>Votre message sera reçu par Simplon Montreuil</p> -->
+            <p>Votre message sera reçu par Simplon Montreuil</p>
             <form method="post">
                 <input type="text" name="Nom" class="Name" placeholder="Nom/Prenom *" required/>
                 <input type="text" name="Nom" class="Name" placeholder="Entreprise *" required/>
@@ -20,4 +67,4 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
--- src/assets/js/controllers/contactCtrl.js
+++ src/assets/js/controllers/contactCtrl.js
@@ -1,3 +1,27 @@
 app.controller('contactCtrl', ['$scope', '$http', 'serviceApi', function($scope, $http, serviceApi) {
     $scope.schools = serviceApi.schools;
+
+
+    function initialize() {
+        var mapProp = {
+            center: new google.maps.LatLng(51.508742, -0.120850),
+            zoom: 16,
+            center: {
+                lat: 48.854491,
+                lng: 2.435967
+            },
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+        var marker = new google.maps.Marker({
+            position: {
+                lat: 48.854491,
+                lng: 2.435967
+            },
+            map: map,
+            title: 'Hello World!',
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 }]);
