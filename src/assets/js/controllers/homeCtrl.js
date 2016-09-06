34b3313681c8c1e6f7d895e1969247e8bc5f21d4
J2A2B <ja.andreanigmail.com>
2016-09-06 16:36:33 +0200

    button home hover

---
+++ src/assets/js/controllers/homeCtrl.js
@@ -0,0 +1,8 @@
+app.controller('homeCtrl', ['$scope', '$http', 'serviceApi', '$route', '$timeout', function($scope, $http, serviceApi, $route, $timeout) {
+$scope.button=true;
+
+$scope.changeButton = function(){
+	$scope.button=false;
+}
+
+}]);
